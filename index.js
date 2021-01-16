02329f32f4fc570649dc3506c3ddf6d0ba7203e9
Evander Kristalino <christrevand@gmail.com>
2021-01-16 13:17:31 +0800

    added heroku port

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 2000
+const port = process.env.PORT || 2000
 
 const { mailerRouter } = require('./router')
 
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use('/mail', mailerRouter)
+
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to API</h1>')
 })
 
-app.use('/mail', mailerRouter)
 
 app.listen(port, () => console.log('Server Running on Heroku Port ' + port))
